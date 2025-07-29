7d7990a170eb8110a89072928bf38fea1a3f5cd6
luz <soporte@grupokonectados.cl>
2025-07-29 15:56:26 -0400

    first commit

---
+++ src/presentation/pages/index.ts
@@ -0,0 +1,7 @@
+export { AssistantPage } from "./assistant/AssistantPage";
+export { AudioToTextPage } from "./audio-to-text/AudioToTextPage";
+export { ImageGenerationPage } from "./image-generation/ImageGenerationPage";
+export { OrthographyPage } from "./orthography/OrthographyPage";
+export { TranslatePage } from "./tanslate/TranslatePage";
+export { TextToAudioPage } from "./text-to-audio/TextToAudioPage";
+
